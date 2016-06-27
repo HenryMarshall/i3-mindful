4903227b499c9acfd196dbd4d66a957788cce381
hbaughman <hbaughman741@gmail.com>
2016-06-27 16:40:18 -0400

    Create timer flow

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const getIdleTime = require('./getIdleTime')
+const questionUser = require('./questionUser')
+const options = require('./options')
 
-getIdleTime(ms => {
-  console.log("ms: ", ms)
-})
+initialize()
+
+function initialize() {
+  timer(Date.now(), () => {
+    questionUser(recallTimer)
+  })
+}
+
+function timer(startTime) {
+  getIdleTime(idleTime => {
+    const currentTime = Date.now()
+
+    if (idleTime >= options.idleReset) {
+      recallTimer()
+    }
+    else if (options.promptInterval <= currentTime - startTime) {
+      callback()
+    }
+    else {
+      recallTimer(startTime)
+    }
+  })
+}
+
+function recallTimer(startTime = Date.now()) {
+  setTimeout(() => {
+    timer(startTime)
+  }, options.loopInterval)
+}
